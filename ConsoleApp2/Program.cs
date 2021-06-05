8b0e31015c734032407bbf870a14e6c98c5a85a2
Nemiseticus <Nemiseticus>
2021-06-05 14:10:33 -0300

    desafio 1-2

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ENTRADA E SAÍDA DE DADOS
+            //LENDO E ESCREVENDO NO CONSOLE
+
+            Console.WriteLine("Olá Mundo/Usuário");
+
+            string texto = "Qual o seu nome: ";
+            //ESCREVER
+            Console.Write(texto);
+            //LER
+            string nome = Console.ReadLine();
+
+            string texto1 = "Qual o seu gênero: ";
+            //ESCREVER
+            Console.Write(texto1);
+            string genero = Console.ReadLine();
+
+            string texto2 = "Qual e sua idade: ";
+            //ESCREVER
+            Console.Write(texto2);
+            string idade = Console.ReadLine();
+
+
+
+
+
+
+
+            Console.WriteLine("Nome: "+nome+ " do gênero: "+ genero+ " e com "+ idade+ " anos de idade.");
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
